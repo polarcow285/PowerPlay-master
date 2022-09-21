2ad82e77ab3c14bbc07425cb329d6ae40d283f08
Natalie Leung <mmoopal@gmail.com>
2022-09-20 17:24:31 -0700

    Coded hardware map and teleop controls for Udon

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectUdon.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.Projects;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+public class ProjectUdon extends Project{
+    //Setup motors
+    public DcMotor frontright = null;
+    public DcMotor frontleft = null;
+    public DcMotor backright = null;
+    public DcMotor backleft = null;
+
+    @Override
+    public void init(HardwareMap ahwMap) {
+        //Define and Initialize Motors
+        frontright = hwMap.dcMotor.get("frontright"); //port c3
+        frontleft = hwMap.dcMotor.get("frontleft"); //port e0
+        backright = hwMap.dcMotor.get("backright"); //port c0
+        backleft = hwMap.dcMotor.get("backleft"); //port e3
+
+        //Setup Motor directions and Encoder settings
+        frontright.setDirection(DcMotor.Direction.FORWARD);
+        frontleft.setDirection(DcMotor.Direction.REVERSE);
+        backright.setDirection(DcMotor.Direction.FORWARD);
+        backleft.setDirection(DcMotor.Direction.REVERSE);
+
+        frontright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        Stop();
+    }
+    public void Stop(){
+        frontright.setPower(0);
+        frontleft.setPower(0);
+        backright.setPower(0);
+        backleft.setPower(0);
+    }
+}
+
