de27fea6a9d56d9e8101cbe010d7821e64fe84f7
1CharlieMartin <ckm91011@gmail.com>
2022-10-27 16:29:13 -0700

    Addsed drive only non driving and manual auto files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DriveOnly.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import org.firstinspires.ftc.teamcode.Projects.ProjectUdon;
+
+@TeleOp(name="DriveOnly", group="Mecanum")
+public class DriveOnly extends LinearOpMode {
+    private ProjectUdon robot = new ProjectUdon();
+    Gamepad currentGamepad2 = new Gamepad();
+    Gamepad previousGamepad2 = new Gamepad();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        telemetry.setAutoClear(true);
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            try {
+                previousGamepad2.copy(currentGamepad2);
+                currentGamepad2.copy(gamepad2);
+            }
+            catch (RobotCoreException e) {
+
+            }
+            //Driving controls
+
+            double y = 0; //back and forth
+            double x = -gamepad1.right_stick_x * 1.1; //strafing
+            double rx = gamepad1.left_stick_x; //turning
+
+            //back and forth movement using triggers
+            if (gamepad1.right_trigger > 0) {
+                y = gamepad1.right_trigger;
+                x = 0;
+            } else if (gamepad1.left_trigger > 0) {
+                y = -gamepad1.left_trigger;
+                x = 0;
+            }
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+
+            robot.frontleft.setPower(frontLeftPower);
+            robot.backleft.setPower(backLeftPower);
+            robot.frontright.setPower(frontRightPower);
+            robot.backright.setPower(backRightPower);
+
+
+
+            idle();
+
+        }
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/NonDriving.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import org.firstinspires.ftc.teamcode.Projects.ProjectUdon;
+
+@TeleOp(name="NonDriving", group="Mecanum")
+public class NonDriving extends LinearOpMode {
+    private ProjectUdon robot = new ProjectUdon();
+    Gamepad currentGamepad2 = new Gamepad();
+    Gamepad previousGamepad2 = new Gamepad();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        telemetry.setAutoClear(true);
+        telemetry.addData("Lift Position", robot.lift.getCurrentPosition());
+
+        int liftTarget = 0;
+        double liftSpeed = 0;
+        String liftCurrentDirection = "up";
+        boolean rollerForwardToggle = false;
+        boolean rollerBackwardToggle = false;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            try {
+                previousGamepad2.copy(currentGamepad2);
+                currentGamepad2.copy(gamepad2);
+            }
+            catch (RobotCoreException e) {
+
+            }
+
+            // roller code
+            if (currentGamepad2.right_bumper && !previousGamepad2.right_bumper) {
+                rollerForwardToggle = !rollerForwardToggle;
+            }
+            if (rollerForwardToggle) {
+                robot.roller.setPower(1);
+            }
+            else {
+                robot.roller.setPower(0);
+            }
+
+            if (currentGamepad2.left_bumper && !previousGamepad2.left_bumper) {
+                rollerBackwardToggle = !rollerBackwardToggle;
+            }
+            if (rollerBackwardToggle) {
+                robot.roller.setPower(-1);
+            }
+            else {
+                robot.roller.setPower(0);
+            }
+
+
+            // lift code
+
+            if (robot.lift.getCurrentPosition()>-2000 && gamepad2.x) { // Arm UP
+                liftTarget = -2000;
+                liftSpeed = 0.98;
+                liftCurrentDirection = "up";
+
+                robot.lift.setPower(liftSpeed);
+                robot.lift.setTargetPosition(liftTarget);
+            }else if (robot.lift.getCurrentPosition()<-2000 && gamepad2.x) { // Arm DOWN
+                liftTarget = -2000;
+                liftSpeed = -0.98;
+                liftCurrentDirection = "down";
+
+                robot.lift.setPower(liftSpeed);
+                robot.lift.setTargetPosition(liftTarget);
+            }else if (robot.lift.getCurrentPosition()>-3500 && gamepad2.b) { // Arm UP
+                liftTarget = -3500;
+                liftSpeed = 0.98;
+                liftCurrentDirection = "up";
+
+                robot.lift.setPower(liftSpeed);
+                robot.lift.setTargetPosition(liftTarget);
+            }else if (robot.lift.getCurrentPosition()<-3500 && gamepad2.b) { // Arm DOWN
+                liftTarget = -3500;
+                liftSpeed = -0.98;
+                liftCurrentDirection = "down";
+
+                robot.lift.setPower(liftSpeed);
+                robot.lift.setTargetPosition(liftTarget);
+            }else if (robot.lift.getCurrentPosition()>-4200 && gamepad2.y) { // Arm UP
+                liftTarget = -4200;
+                liftSpeed = 0.98;
+                liftCurrentDirection = "up";
+
+                robot.lift.setPower(liftSpeed);
+                robot.lift.setTargetPosition(liftTarget);
+            }else if (robot.lift.getCurrentPosition()<-4200 && gamepad2.y) { // Arm DOWN
+                liftTarget = -4200;
+                liftSpeed = -0.98;
+                liftCurrentDirection = "down";
+
+                robot.lift.setPower(liftSpeed);
+                robot.lift.setTargetPosition(liftTarget);
+            } else if (gamepad2.a){ // Arm DOWN
+                liftTarget = 0;
+                liftSpeed = -0.98;  // From my research, negative is ignore, so I don't understand why this *seemed* to work
+                liftCurrentDirection = "down";
+
+                robot.lift.setPower(liftSpeed);
+                robot.lift.setTargetPosition(liftTarget);
+            }
+
+            // Remove Power from the Arm Motor if motor is close to 0 position, arm should drop
+//            if ( liftCurrentDirection == "down" && ( lift.getTargetPosition() < 5 ) ){
+//                liftSpeed = 0;
+//                lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            }
+
+            /** END ARM LIFT **/
+
+
+            idle();
+
+            // Arm Lift Telemetry
+            if(robot.lift.isBusy() ){
+                telemetry.addData("Lift Position", robot.lift.getCurrentPosition());
+                telemetry.update();
+            }
+
+        }
+
+    }
+
+}
