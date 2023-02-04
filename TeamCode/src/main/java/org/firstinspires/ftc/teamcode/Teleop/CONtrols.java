3631a8cb98e1827efbbcc1f08c6812b1a6087d44
Natalie Leung <mmoopal@gmail.com>
2023-02-03 19:17:11 -0800

    Reverted CONtrols to meet 3 code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/CONtrols.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/CONtrols.java
@@ -134,7 +134,7 @@ public class CONtrols extends LinearOpMode {
 
             // lift code
 
-            if (robot.lift.getCurrentPosition()>-2000 && gamepad2.x) { // Arm UP
+            /*if (robot.lift.getCurrentPosition()>-2000 && gamepad2.x) { // Arm UP
                 liftTarget = -2000;
                 liftSpeed = 0.98;
                 liftCurrentDirection = "up";
@@ -184,7 +184,7 @@ public class CONtrols extends LinearOpMode {
                 robot.lift.setPower(liftSpeed);
                 robot.lift.setTargetPosition(liftTarget);
             }
-
+            */
             // Remove Power from the Arm Motor if motor is close to 0 position, arm should drop
 //            if ( liftCurrentDirection == "down" && ( robot.lift.getTargetPosition() < 5 ) ){
 //                liftSpeed = 0;
@@ -194,7 +194,7 @@ public class CONtrols extends LinearOpMode {
             /** END ARM LIFT **/
 
             //Outtake sequence
-            if(gamepad2.dpad_down && robot.lift.getCurrentPosition()>50){
+            /*if(gamepad2.dpad_down && robot.lift.getCurrentPosition()>50){
                 liftTarget = robot.lift.getCurrentPosition()-50;
                 liftSpeed = -0.98;
                 liftCurrentDirection = "down";
@@ -209,7 +209,7 @@ public class CONtrols extends LinearOpMode {
 
 
             idle();
-
+            */
 
 
 
